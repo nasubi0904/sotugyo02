5d2cb513cd6557ddb3eb6543b0cfe3a4dfc2c356
nasubi0904 <shin040904@gmail.com>
2026-01-16 09:25:25 +0900

    プラグイン管理ダイアログのUIを追加

--- src/sotugyo/ui/dialogs/__init__.py
+++ src/sotugyo/ui/dialogs/__init__.py
@@ -2,12 +2,14 @@
 
 from __future__ import annotations
 
+from .plugin_manager_dialog import PluginManagerDialog
 from .project_settings_dialog import ProjectSettingsDialog
 from .tool_environment_dialog import ToolEnvironmentManagerDialog
 from .tool_registry_dialog import ToolRegistryDialog
 from .user_settings_dialog import UserSettingsDialog
 
 __all__ = [
+    "PluginManagerDialog",
     "ProjectSettingsDialog",
     "ToolEnvironmentManagerDialog",
     "ToolRegistryDialog",
