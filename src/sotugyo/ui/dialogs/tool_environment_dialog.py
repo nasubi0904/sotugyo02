bc1333358419190178a1e55222e7baa3856b5401
nasubi0904 <shin040904@gmail.com>
2026-01-20 15:54:52 +0900

    要求ファイル見出しを和英併記へ

--- src/sotugyo/ui/dialogs/tool_environment_dialog.py
+++ src/sotugyo/ui/dialogs/tool_environment_dialog.py
@@ -205,7 +205,7 @@ class ToolEnvironmentEditorDialog(QDialog):
 
         layout.addLayout(form)
 
-        plugin_label = QLabel("Required Files (Plugin,module,etc...)", self)
+        plugin_label = QLabel("要求ファイルの設定 (Plugin,module,etc...)", self)
         layout.addWidget(plugin_label)
 
         plugin_layout = QHBoxLayout()
